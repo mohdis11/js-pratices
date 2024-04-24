e9f6405ba2ea2f9f87ac2ae321a96ca17f1f5aec
mohdis11 <118658524+mohdis11@users.noreply.github.com>
2024-04-24 16:16:46 +0000

    CoversionOperation done

---
+++ JS_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version.
+
+// "alert(3+3)" we only use this in broswer / not in node js
+
+console.log(3+3); // we can as follows in node js 
+
+console.log(3
+    +3); // this is not good code readiablity,  so the code readiablity should be high.
+
+    let name="ismail"
+    let age=20
+    let isLoggedIn=true
+    let state;
+
+/* there are many datatypes in javaScript as follows...
+
+primitive datatpyes:-
+
+1 number   => 2 to power 55 
+2 bigint   => we use this rarely, mainly when we need store big number.
+3 Sring    => we use this in "" or '' but preferable is  "".
+4 boolean  => true/False.
+5 null     => it is a standalone value.
+6 undefine => when value is not defined yet.
+7 symbol   => to find uniqueness
+
+
+non primitive datatypes:-
+
+1 objects => 
+*/
+
+
+console.log(typeof null);
---
+++ JS_basics/03_conversionOperations.js
@@ -0,0 +1,95 @@
+/*let score = undefined
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNumber = Number(score)
+
+console.log(typeof valueNumber);
+console.log(valueNumber)
+
+
+/*
+notes :- 
+    conversion from     value   to  number
+        number           33          33
+        String           33adbd      NaN
+        Boolean          true        1
+        boolean          false       0
+        null             null        0
+        undefine         undefine    NaN
+*/
+/*
+let isLoggedIn = ""
+
+console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
+/*
+notes :-
+    
+    conversion from     value   to  boolean
+        number           1          true
+                         0          false
+                         "ismail"   true
+                         ""         false
+*/ 
+/*
+let num = ""
+
+console.log(typeof num)
+console.log(num)
+
+let str = String(num)
+
+console.log(typeof str);
+console.log(str);
+
+/*
+notes:-
+    conversion from     value   to  String
+        number           75          75 
+                         true        true
+                         false       false
+*/
+
+
+
+
+/******************************************************** Operations ************************************************************/
+
+
+let num = 3
+let neganum = -num
+console.log(neganum);
+
+// similarly some common operations which used in any lauguage
+/*console.log(2+2); Addition 
+console.log(2-2);   Substraction
+console.log(2*2);   multiplication
+console.log(2**2);  expotential
+console.log(2/3);   division and give answer as quotient
+console.log(2%3);   division and give answer as remainder
+*/
+
+
+
+// some important operation with some difference in every language 
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 ); // In expression if string then the result shoulb be comes in string datatypes
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");// In this form the complier convert the expression from left to right
+
+console.log((3 + 4) * 6 % 5); // In this we mainly use pedmas rule in any language
+
+
