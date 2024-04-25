42d95315907e9fce87137095185fc42a0f2ba625
mohdis11 <118658524+mohdis11@users.noreply.github.com>
2024-04-25 13:09:38 +0000

    jump into datatype summary now

--- JS_basics/04_comparison.js
+++ JS_basics/04_comparison.js
@@ -1,6 +1,6 @@
 // A normal comparison in every lauguage i.e, a basics comparison we always do
 
-console.log(2 == 1);
+console.log(2 == 2);
 console.log(2 <= 1);
 console.log(2 < 1);
 console.log(2 >= 1);
@@ -15,3 +15,23 @@ console.log(3 > "2");
 
 // similar operation we ==, !=, >=, <= we get the actual answer we needed where the above line give the out we need 
 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null != 0);
+console.log(null < 0);
+console.log(null <= 0);
+
+console.log(undefined < 0);
+console.log(undefined <= 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+console.log(undefined != 0);
+
+// useage a the above 12 lines we need avoid if want the code to be clean
+
+
+console.log(2 === "2");
+
+//if we use "==" we just compare the value and if we use  "===" then we are going to compare the value with datatype 
