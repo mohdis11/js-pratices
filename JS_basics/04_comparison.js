36b736c31bf578c5777707250a367350be1a2e21
mohdis11 <118658524+mohdis11@users.noreply.github.com>
2024-04-25 12:46:26 +0000

    comit

---
+++ JS_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// A normal comparison in every lauguage i.e, a basics comparison we always do
+
+console.log(2 == 1);
+console.log(2 <= 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 > 1);
+console.log(2 != 1);
+
+
+// some comparison that actually Mainly in JS 
+
+console.log("1" < 2);
+console.log(3 > "2");
+
+// similar operation we ==, !=, >=, <= we get the actual answer we needed where the above line give the out we need 
+
